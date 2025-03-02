ebc13665f20d51a0b981c951273b8e3a5575733d
William Zhang <wzhang20@yahoo.com>
2025-03-02 13:47:55 -0500

    attempt lqr

--- lqr-balancer.py
+++ lqr-balancer.py
@@ -42,13 +42,13 @@ height_offsets = np.linspace(-0.001, 0.001, 2001)
 
 vertical_forces = []
 for offset in height_offsets:
-  mujoco.mj_resetDataKeyframe(model, data, 1)
-  mujoco.mj_forward(model, data)
-  data.qacc = 0
-  # Offset the height by `offset`.
-  data.qpos[2] += offset
-  mujoco.mj_inverse(model, data)
-  vertical_forces.append(data.qfrc_inverse[2])
+    mujoco.mj_resetDataKeyframe(model, data, 1)
+    mujoco.mj_forward(model, data)
+    data.qacc = 0
+    # Offset the height by `offset`.
+    data.qpos[2] += offset
+    mujoco.mj_inverse(model, data)
+    vertical_forces.append(data.qfrc_inverse[2])
 
 idx = np.argmin(np.abs(vertical_forces))
 best_offset = height_offsets[idx]
@@ -100,19 +100,129 @@ print('actuator forces:', data.qfrc_actuator)
 
 frames = []
 while data.time < DURATION:
-  # Step the simulation.
-  mujoco.mj_step(model, data)
+    # Step the simulation.
+    mujoco.mj_step(model, data)
 
-  # Render and save frames.
-  if len(frames) < data.time * FRAMERATE:
-    # Set the lookat point to the humanoid's center of mass.
-    camera.lookat = data.body('torso').subtree_com
-    renderer.update_scene(data, camera)
-    pixels = renderer.render()
-    frames.append(pixels)
+    # Render and save frames.
+    if len(frames) < data.time * FRAMERATE:
+        # Set the lookat point to the humanoid's center of mass.
+        camera.lookat = data.body('torso').subtree_com
+        renderer.update_scene(data, camera)
+        pixels = renderer.render()
+        frames.append(pixels)
 
+"""Result: doesn't work in long term...weight pushes down"""
 imageio.mimsave("feedforward-ctrl.mp4", frames, fps=FRAMERATE)
 
 #------------------------
 # Using Linear Quadratic Regulator
 #-------------------------
+
+nu = model.nu  # Alias for the number of actuators.
+R = np.eye(nu)
+
+nv = model.nv  # Shortcut for the number of DoFs.
+
+# Get the Jacobian for the root body (torso) CoM.
+mujoco.mj_resetData(model, data)
+data.qpos = qpos0
+mujoco.mj_forward(model, data)
+jac_com = np.zeros((3, nv))
+mujoco.mj_jacSubtreeCom(model, data, jac_com, model.body('torso').id)
+
+# Get the Jacobian for the left foot.
+jac_foot = np.zeros((3, nv))
+mujoco.mj_jacBodyCom(model, data, jac_foot, None, model.body('foot_left').id)
+
+jac_diff = jac_com - jac_foot
+Qbalance = jac_diff.T @ jac_diff
+
+# Get all joint names.
+joint_names = [model.joint(i).name for i in range(model.njnt)]
+
+# Get indices into relevant sets of joints.
+root_dofs = range(6)
+body_dofs = range(6, nv)
+abdomen_dofs = [
+    model.joint(name).dofadr[0]
+    for name in joint_names
+    if 'abdomen' in name
+    and not 'z' in name
+]
+left_leg_dofs = [
+    model.joint(name).dofadr[0]
+    for name in joint_names
+    if 'left' in name
+    and ('hip' in name or 'knee' in name or 'ankle' in name)
+    and not 'z' in name
+]
+balance_dofs = abdomen_dofs + left_leg_dofs
+other_dofs = np.setdiff1d(body_dofs, balance_dofs)
+
+# Cost coefficients.
+BALANCE_COST        = 1000  # Balancing.
+BALANCE_JOINT_COST  = 3     # Joints required for balancing.
+OTHER_JOINT_COST    = .3    # Other joints.
+
+# Construct the Qjoint matrix.
+Qjoint = np.eye(nv)
+Qjoint[root_dofs, root_dofs] *= 0  # Don't penalize free joint directly.
+Qjoint[balance_dofs, balance_dofs] *= BALANCE_JOINT_COST
+Qjoint[other_dofs, other_dofs] *= OTHER_JOINT_COST
+
+# Construct the Q matrix for position DoFs.
+Qpos = BALANCE_COST * Qbalance + Qjoint
+
+# No explicit penalty for velocities.
+Q = np.block([[Qpos, np.zeros((nv, nv))],
+              [np.zeros((nv, 2*nv))]])
+
+##########################
+# Computing the K Matrix #
+# Set the initial state and control.
+
+mujoco.mj_resetData(model, data)
+data.ctrl = ctrl0
+data.qpos = qpos0
+
+# Allocate the A and B matrices, compute them.
+A = np.zeros((2*nv, 2*nv))
+B = np.zeros((2*nv, nu))
+epsilon = 1e-6
+flg_centered = True
+mujoco.mjd_transitionFD(model, data, epsilon, flg_centered, A, B, None, None)
+
+# Solve discrete Riccati equation (using SciPy library call for Algebraic Riccati Equation - hence ARE)
+P = scipy.linalg.solve_discrete_are(A, B, Q, R)
+
+# Compute the feedback gain matrix K.
+K = np.linalg.inv(R + B.T @ P @ B) @ B.T @ P @ A
+
+DURATION = 5          # seconds
+
+# Reset data, set initial pose.
+mujoco.mj_resetData(model, data)
+data.qpos = qpos0
+
+# Allocate position difference dq.
+dq = np.zeros(model.nv)
+
+frames = []
+while data.time < DURATION:
+  # Get state difference dx.
+  mujoco.mj_differentiatePos(model, dq, 1, qpos0, data.qpos)
+  dx = np.hstack((dq, data.qvel)).T
+
+  # LQR control law.
+  data.ctrl = ctrl0 - K @ dx
+
+  # Step the simulation.
+  mujoco.mj_step(model, data)
+
+  # Render and save frames.
+  if len(frames) < data.time * FRAMERATE:
+    renderer.update_scene(data)
+    pixels = renderer.render()
+    frames.append(pixels)
+
+imageio.mimsave("lqr-standup.mp4", frames, fps=FRAMERATE)
