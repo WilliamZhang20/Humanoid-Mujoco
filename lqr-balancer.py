921b816813af52fdee7ceb9b61b842979a8185dc
William Zhang <wzhang20@yahoo.com>
2025-03-02 12:47:33 -0500

    finish feedforward control

--- lqr-balancer.py
+++ lqr-balancer.py
@@ -27,6 +27,77 @@ FRAMERATE = 60  # Hz
 # Initialize to the standing-on-one-leg pose.
 mujoco.mj_resetDataKeyframe(model, data, 1)
 
+#----------------------------
+# Special Camera Angle
+#---------------------------
+
+camera = mujoco.MjvCamera()
+mujoco.mjv_defaultFreeCamera(model, camera)
+camera.distance = 2
+
+#----------------------------
+# Examining Force Dynamics
+#----------------------------
+height_offsets = np.linspace(-0.001, 0.001, 2001)
+
+vertical_forces = []
+for offset in height_offsets:
+  mujoco.mj_resetDataKeyframe(model, data, 1)
+  mujoco.mj_forward(model, data)
+  data.qacc = 0
+  # Offset the height by `offset`.
+  data.qpos[2] += offset
+  mujoco.mj_inverse(model, data)
+  vertical_forces.append(data.qfrc_inverse[2])
+
+idx = np.argmin(np.abs(vertical_forces))
+best_offset = height_offsets[idx]
+
+# Plot the relationship.
+plt.figure(figsize=(10, 6))
+plt.plot(height_offsets * 1000, vertical_forces, linewidth=3)
+# Red vertical line at offset corresponding to smallest vertical force.
+plt.axvline(x=best_offset*1000, color='red', linestyle='--')
+# Green horizontal line at the humanoid's weight.
+weight = model.body_subtreemass[1]*np.linalg.norm(model.opt.gravity)
+plt.axhline(y=weight, color='green', linestyle='--')
+plt.xlabel('Height offset (mm)')
+plt.ylabel('Vertical force (N)')
+plt.grid(which='major', color='#DDDDDD', linewidth=0.8)
+plt.grid(which='minor', color='#EEEEEE', linestyle=':', linewidth=0.5)
+plt.minorticks_on()
+plt.title(f'Smallest vertical force '
+          f'found at offset {best_offset*1000:.4f}mm.')
+plt.show()
+
+mujoco.mj_forward(model, data)
+data.qacc = 0
+data.qpos[2] += best_offset
+qpos0 = data.qpos.copy()  # Save the position setpoint.
+mujoco.mj_inverse(model, data)
+qfrc0 = data.qfrc_inverse.copy()
+print('desired forces:', qfrc0)
+
+#----------------
+# Attempt a feedforward control law
+#----------------
+
+actuator_moment = np.zeros((model.nu, model.nv))
+mujoco.mju_sparse2dense(
+    actuator_moment,
+    data.actuator_moment.reshape(-1),
+    data.moment_rownnz,
+    data.moment_rowadr,
+    data.moment_colind.reshape(-1),
+)
+ctrl0 = np.atleast_2d(qfrc0) @ np.linalg.pinv(actuator_moment) # apply at setpoint using inverse dynamics!
+ctrl0 = ctrl0.flatten()  # Save the ctrl setpoint.
+print('control setpoint:', ctrl0)
+
+data.ctrl = ctrl0
+mujoco.mj_forward(model, data)
+print('actuator forces:', data.qfrc_actuator)
+
 frames = []
 while data.time < DURATION:
   # Step the simulation.
@@ -34,9 +105,14 @@ while data.time < DURATION:
 
   # Render and save frames.
   if len(frames) < data.time * FRAMERATE:
-    renderer.update_scene(data)
+    # Set the lookat point to the humanoid's center of mass.
+    camera.lookat = data.body('torso').subtree_com
+    renderer.update_scene(data, camera)
     pixels = renderer.render()
     frames.append(pixels)
 
-# Display video.
-imageio.mimsave("output.mp4", frames, fps=FRAMERATE)
+imageio.mimsave("feedforward-ctrl.mp4", frames, fps=FRAMERATE)
+
+#------------------------
+# Using Linear Quadratic Regulator
+#-------------------------
