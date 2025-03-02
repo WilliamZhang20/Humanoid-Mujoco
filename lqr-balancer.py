f45f81edbfca48caad22b54d74aca0407212d5a3
William Zhang <wzhang20@yahoo.com>
2025-03-02 12:20:33 -0500

    get through initial experiments

--- lqr-balancer.py
+++ lqr-balancer.py
@@ -2,6 +2,9 @@ import numpy as np
 from typing import Callable, Optional, Union, List
 import scipy.linalg
 
+import imageio
+import matplotlib.pyplot as plt
+
 try:
     print('Checking that the installation succeeded:')
     import mujoco
@@ -13,3 +16,27 @@ except Exception as e:
 
 with open('humanoid.xml', 'r') as f:
     xml = f.read()
+
+model = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model)
+renderer = mujoco.Renderer(model)
+
+DURATION  = 3   # seconds
+FRAMERATE = 60  # Hz
+
+# Initialize to the standing-on-one-leg pose.
+mujoco.mj_resetDataKeyframe(model, data, 1)
+
+frames = []
+while data.time < DURATION:
+  # Step the simulation.
+  mujoco.mj_step(model, data)
+
+  # Render and save frames.
+  if len(frames) < data.time * FRAMERATE:
+    renderer.update_scene(data)
+    pixels = renderer.render()
+    frames.append(pixels)
+
+# Display video.
+imageio.mimsave("output.mp4", frames, fps=FRAMERATE)
